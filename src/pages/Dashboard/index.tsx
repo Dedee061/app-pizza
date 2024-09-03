2623f1a08655fd1009cf1bce26a14fa12e1bc70d
Andre Santos <andresantoss0598@gmail.com>
2024-09-02 21:43:31 -0300

    finalizando tela de dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import React, { useContext } from "react";
 
 import { AuthContext } from "../../contexts/AuthContext";
@@ -8,17 +8,46 @@ export default function Dashboard() {
   return (
     <View style={styles.container}>
       
-      <Text>Tela Dashboard</Text>
+      <Text>Abrir Mesa</Text>
+
+      <TextInput  placeholder="Numero da mesa" placeholderTextColor={"#fff7"} style={styles.input}/>
+
+      <TouchableOpacity style={styles.button}>
+        <Text>Abrir Mesa</Text>
+      </TouchableOpacity>
+      
     </View>
   );
 
   }
 
   const styles = StyleSheet.create({
-    container:{
+    container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#1D1D2E'
-    }
+      backgroundColor: "#1D1D2E",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    input: {
+      width: "95%",
+      height: 60,
+      fontSize: 20,
+      backgroundColor: "#101026",
+      marginBottom: 10,
+      paddingHorizontal: 10,
+      color: "#fff",
+      borderRadius: 8,
+      borderWidth: 0.7,
+      borderColor: "#8A8A8A",
+    },
+    button: {
+      width: "95%",
+      height: 40,
+      backgroundColor: "#3FFFA3",
+      justifyContent: "center",
+      alignItems: "center",
+      fontWeight: 'bold',
+      borderRadius: 8,
+      marginTop: 20,
+    },
   })
