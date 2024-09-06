eeae97198deac35fae6d10f4e59ce591d85ad7c9
Andre Santos <andresantoss0598@gmail.com>
2024-09-05 23:53:39 -0300

    configurando tipagem e tela de dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,65 +1,92 @@
-import { View, Text, Button, StyleSheet, TouchableOpacity, TextInput, SafeAreaView } from "react-native";
-import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
+import React, { useContext, useState } from "react";
 
 import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import { StackPramsLst } from "../../routes/app.routes";
+
 
 export default function Dashboard() {
+  const navigation = useNavigation<NativeStackNavigationProp<StackPramsLst>>()
   const { signOut } = useContext(AuthContext);
-  return (
-    <SafeAreaView style={styles.container}>
-      
-      <Text style={styles.text}>Novo Pedido</Text>
+  const [number, setNumber] = useState("");
+  
 
-      <TextInput keyboardType="numeric" placeholder="Numero da mesa" placeholderTextColor={"#fff7"} style={styles.input}/>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.textButton} >Abrir Mesa</Text>
-      </TouchableOpacity>
-      
-    </SafeAreaView>
-  );
+  async function OpenOrder(){
+    if(number === '' || number === null) {
+      return;
+    }
+    navigation.navigate('Order')
 
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#1D1D2E",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    input: {
-      width: "90%",
-      height: 60,
-      fontSize: 20,
-      backgroundColor: "#101026",
-      paddingHorizontal: 10,
-      color: "#fff",
-      borderRadius: 8,
-      borderWidth: 0.7,
-      borderColor: "#8A8A8A",
-      textAlign: 'center'
-    },
-    button: {
-      width: "90%",
-      height: 40,
-      backgroundColor: "#3FFFA3",
-      justifyContent: "center",
-      alignItems: "center",
-      fontWeight: 'bold',
-      borderRadius: 8,
-      marginTop: 20,
-    },
-    textButton:{
-      fontWeight: 'bold',
-    }, 
-    text:{
-      fontSize: 45,
-      marginBottom: 25,
-      color: "#fff",
-      paddingHorizontal: 10,
-      fontWeight: 'bold'
-      
-      
-    }
-  })
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Novo Pedido</Text>
+
+      <TextInput
+        keyboardType="numeric"
+        placeholder="Numero da mesa"
+        placeholderTextColor={"#fff7"}
+        style={styles.input}
+        value={number}
+        onChangeText={setNumber}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={OpenOrder}>
+        <Text style={styles.textButton}>Abrir Mesa</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1D1D2E",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "90%",
+    height: 60,
+    fontSize: 20,
+    backgroundColor: "#101026",
+    paddingHorizontal: 10,
+    color: "#fff",
+    borderRadius: 8,
+    borderWidth: 0.7,
+    borderColor: "#8A8A8A",
+    textAlign: "center",
+  },
+  button: {
+    width: "90%",
+    height: 40,
+    backgroundColor: "#3FFFA3",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "bold",
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  textButton: {
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 45,
+    marginBottom: 25,
+    color: "#fff",
+    paddingHorizontal: 10,
+    fontWeight: "bold",
+  },
+});
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,13 +3,21 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Dashboard from '../pages/Dashboard'
+import Order from '../pages/Order'
 
-const Stack = createNativeStackNavigator()
+
+export type StackPramsLst = {
+  Dashboard: undefined,
+  Order: undefined
+}
+
+const Stack = createNativeStackNavigator<StackPramsLst>()
 
 export default function AppRoutes() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Dashboard" component={Dashboard} options={{headerShown: false}} />
+      <Stack.Screen name="Order" component={Order} options={{headerShown: false}} />
     </Stack.Navigator>
   )
 }
